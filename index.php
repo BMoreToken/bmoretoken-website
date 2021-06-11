fe535416083827aaa2932c653582a56c8368fd7f
Patrick McGuire <insidenothing@gmail.com>
2021-06-11 15:41:16 -0400

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ include_once('menu.php');
  <p>Max Total Supply: 3,947,960 BALT</p>
  <p>Current Circulation: 8,117.07 BALT</p>
  <p>Holders: 6</p>
- <p>Transfers: 9</p>
+ <p>Transfers: 10</p>
  <p>Address: 0xbdd4f273c2b0f0b84a51bd733aac617d91159376</p>
 </pre>
  </center>
