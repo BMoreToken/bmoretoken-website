43c37759c36997d62cbed81866ebe103538604af
Patrick McGuire <insidenothing@gmail.com>
2021-06-12 15:17:57 -0400

    Update distribution.php

--- distribution.php
+++ distribution.php
@@ -30,26 +30,44 @@ var chart = new CanvasJS.Chart("chartContainer", {
 			{ y: 8117.07, label: "Immediate Trading" },
 			{ y: 1000000.00, label: "Future Trading" },
 			{ y: 100000, label: "Bounties" },
-			{ y: 1000000.00, label: "Community Projects" },
+			{ y: 1500000.00, label: "Community Projects and Airdrops" },
 			{ y: 900000, label: "Team" },
-			{ y: 947960, label: "Owner" }
+			{ y: 447960, label: "Owner" }
 		]
 	}]
 });
 chart.render();
-
+var chart2 = new CanvasJS.Chart("chartContainer2", {
+	theme: "light2", // "light1", "light2", "dark1", "dark2"
+	exportEnabled: true,
+	animationEnabled: true,
+	title: {
+		text: "Actual Distribution"
+	},
+	data: [{
+		type: "pie",
+		startAngle: 25,
+		toolTipContent: "<b>{label}</b>: {y}",
+		showInLegend: "true",
+		legendText: "{label}",
+		indexLabelFontSize: 16,
+		indexLabel: "{label} - {y}",
+		dataPoints: [
+			{ y: 7967.07, label: "Trading" }
+		]
+	}]
+});
+chart2.render();
 }
 </script>
 
-<div id="chartContainer" style="height: 75%; width: 100%;"></div>
+<div id="chartContainer" style="height: 400px; width: 100%;"></div>
+<div id="chartContainer2" style="height: 400px; width: 100%;"></div>
 <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
 
  
  
- 
- 
- 
- 
+  
  <img src='dist.jpg' class="img-fluid" alt="Responsive image">
   
  </center> 
