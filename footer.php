7c8af819daa01af859558e808bf3d8ef5f2dfb38
Patrick McGuire <insidenothing@gmail.com>
2021-06-12 08:14:09 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,4 @@
-    <center><div class="fb-share-button" data-href="https://www.bmoretoken.com<?PHP echo $_SERVER['PHP_SELF'];?>" data-layout="box_count" data-size="large"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.bmoretokin.com<?PHP echo urlencode($_SERVER['PHP_SELF']);?>&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div><br>
-    <br> &copy; 2021 BMoreToken</center>
+    <center>&copy; 2021 BMoreToken</center>
     <!-- Go to www.addthis.com/dashboard to customize your tools -->
     <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-60c4a3c7c7a262c8"></script>
   </body>
