ab80b2eae1fac6c4d7b919a9c949ef8fdbf22e4d
Patrick McGuire <insidenothing@gmail.com>
2021-06-12 14:00:15 -0400

    Create daily_liquidity.php

---
+++ daily_liquidity.php
@@ -0,0 +1,9 @@
+<?PHP
+include_once("menu.php");
+?>
+<center>
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/HE1H5G_w0hY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</center>
+<?PHP
+include_once("footer.php");
+?>
