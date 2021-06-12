e30ccc6957e3e9b465096dbf369777c1a804f6ee
Patrick McGuire <insidenothing@gmail.com>
2021-06-12 13:47:22 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,8 +1,6 @@
     <center>&copy; 2021 BMoreToken<br>
     <!-- Go to www.addthis.com/dashboard to customize your tools -->
     <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-60c4a3c7c7a262c8"></script>
-    <script type="text/javascript" src="https://geoloc2.geovisite.ovh/private/geocounter.js?compte=qzzazb6w5j82"></script> 
-    <img class="img-fluid" alt="Responsive image" src="https://geoloc2.geovisite.ovh/private/geoglobe.php?compte=qzzazb6w5j82&base=geoloc2"><br>
-    <img class="img-fluid" alt="Responsive image" src="https://geoloc2.geovisite.ovh/private/geocounter.php?compte=qzzazb6w5j82&base=geoloc2"></center>
+    <script type="text/javascript" id="clstr_globe" src="//clustrmaps.com/globe.js?d=rOs-HkM_Hp0stTGGz-5TsyrqOCrbsfsY5KznLAgweRE"></script></center>
   </body>
 </html>
