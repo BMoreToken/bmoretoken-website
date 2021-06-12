ed4d3dd810005af771548de5622ad8190d9b38b6
Patrick McGuire <insidenothing@gmail.com>
2021-06-12 16:24:01 -0400

    Update get_balt.php

--- get_balt.php
+++ get_balt.php
@@ -2,13 +2,17 @@
 include_once("menu.php");
 ?>
 <div class="container">
-<form method="POST" action="https://www.bmorecoin.com/request_balt.php">
-  <div class="form-group" height="50%">
-    <label for="exampleFormControlFile1">Enter your ERC20 Address Below</label><br>
-    <input type="text" minlength="42" maxlength="42" id="address" name="address"><br>
-    <input class="btn btn-success" name='Get 1 BALT for FREE this Friday!' type="Submit">
+  <div class="row justify-content-md-center">
+    <div class="col-md-auto">
+      <form method="POST" action="https://www.bmorecoin.com/request_balt.php">
+        <div class="form-group" height="50%">
+          <label for="exampleFormControlFile1">Enter your ERC20 Address Below</label><br>
+          <input type="text" minlength="42" maxlength="42" id="address" name="address"><br>
+          <input class="btn btn-success" name='Get 1 BALT for FREE this Friday!' type="Submit">
+        </div>
+      </form> 
+    </div>
   </div>
-</form>
 </div>
 <?PHP
 include_once("footer.php");
