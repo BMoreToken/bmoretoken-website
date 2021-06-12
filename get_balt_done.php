7bebb488ed9df1bb3ad107adda305a5454c8abd7
Patrick McGuire <insidenothing@gmail.com>
2021-06-12 16:01:30 -0400

    Update get_balt_done.php

--- get_balt_done.php
+++ get_balt_done.php
@@ -1,9 +1,17 @@
 <?PHP
 include_once("menu.php");
+if (isset($_GET['drop'])){ 
+  // for all good security reasons dont actually use $_GET['drop']
 ?>
 <center>
   Address Added for Next Friday's Drop. Thanks. Spread the Word!!!
 </center>
 <?PHP
+}else{
+?>
+<center>
+  Address **NOT** Added for Next Friday's Drop. Maybe Try Again?
+</center>
+<?PHP}
 include_once("footer.php");
 ?>
