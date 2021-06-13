863b98537fb526176edb0906f1b32e71304cffc1
Patrick McGuire <insidenothing@gmail.com>
2021-06-13 07:49:57 -0400

    back to default

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
         <!-- Go to www.addthis.com/dashboard to customize -->
         <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-60c4a3c7c7a262c8"></script>
         <!-- Go to https://clustrmaps.com/profile to customize -->
-        <script type='text/javascript' id='clustrmaps' src='//cdn.clustrmaps.com/map_v2.js?cl=827979&w=a&t=m&d=rOs-HkM_Hp0stTGGz-5TsyrqOCrbsfsY5KznLAgweRE&co=ffffff&ct=000000'></script>
+        <script type='text/javascript' id='clustrmaps' src='//cdn.clustrmaps.com/map_v2.js?cl=ffffff&w=a&t=m&d=rOs-HkM_Hp0stTGGz-5TsyrqOCrbsfsY5KznLAgweRE'></script>
     </center>
   </body>
 </html>
