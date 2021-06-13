a0e509e30ec2974d51bed32b37ba8de2e3633908
Patrick McGuire <insidenothing@gmail.com>
2021-06-13 07:44:32 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,9 @@
-    <center>&copy; 2021 BMoreToken<br>
-    <!-- Go to www.addthis.com/dashboard to customize your tools -->
-    <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-60c4a3c7c7a262c8"></script>
-    <script type='text/javascript' id='clustrmaps' src='//cdn.clustrmaps.com/map_v2.js?cl=0e1633&w=300&t=n&d=rOs-HkM_Hp0stTGGz-5TsyrqOCrbsfsY5KznLAgweRE&co=ffffff&cmo=3acc3a&cmn=ff5353&ct=cdd4d9'></script></center>
+    <center>
+        &copy; 2021 BMoreToken<br>
+        <!-- Go to www.addthis.com/dashboard to customize -->
+        <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-60c4a3c7c7a262c8"></script>
+        <!-- Go to https://clustrmaps.com/profile to customize -->
+        <script type='text/javascript' id='clustrmaps' src='//cdn.clustrmaps.com/map_v2.js?cl=827979&w=a&t=m&d=rOs-HkM_Hp0stTGGz-5TsyrqOCrbsfsY5KznLAgweRE&co=ffffff&ct=000000'></script>
+    </center>
   </body>
 </html>
