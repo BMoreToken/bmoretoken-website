6577a91d22fe57ad21dc31e13ef81a31fa4b1f3f
Patrick McGuire <insidenothing@gmail.com>
2021-06-14 06:36:35 -0400

    Update bounties.php

--- bounties.php
+++ bounties.php
@@ -2,13 +2,14 @@
 <div class="container">
  <center>    
    <div class="row justify-content-md-center">
-     <h1>Paid Offers:</h1>
+     <h1>Paid Offers: ( lack of time or skill so we outsource )</h1>
        <h3>New Logo Set 1500x200 500x500, 256x256, 32x32</h3>
        <h3>Static Website Redesign, bootstrap@5</h3>
        <h3>Edit Project Writeup ~500 words</h3>
        <h3>Edit Project Tagline ~20 words</h3>
        <h3>Referral of new team member</h3>
        <h3>Promotions</h3>   
+       <h3>Start a proper BitcoinTalk thread</h3>
     </div>
  </center>
 </div>
