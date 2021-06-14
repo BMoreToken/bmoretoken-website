126abfeb54e68bad8631276a7b221911b6ee6a1a
Patrick McGuire <insidenothing@gmail.com>
2021-06-14 06:17:59 -0400

    Update team.php

--- team.php
+++ team.php
@@ -2,23 +2,18 @@
 
 
 <div class="container">
- 
-<center>    
- <img src='logo.png' class="img-fluid" alt="Responsive image">
- </center>
 
-    <div class="row justify-content-md-center">
-     <div class="col-md-auto">
-       <a type="button" class="btn btn-info btn-lg btn-block" href='https://www.linkedin.com/in/baltimorehacker'>Patrick McGuire<br>Finance and Technology</a>
-      </div>
-    </div>
-  <br><br>
-      <div class="row justify-content-md-center">
-     <div class="col-md-auto">
-       <a type="button" class="btn btn-warning btn-lg btn-block" href='contact.php'>Join</a>
-      </div>
-    </div>
-  
+ 
+ 
+ <table>
+  <tr>
+   <td><img src="1537101314639.jpg"></td>
+   <td><a type="button" class="btn btn-info btn-lg btn-block" href='https://www.linkedin.com/in/baltimorehacker'>Patrick McGuire<br>Finance and Technology</a></td>
+  </tr>
+ </table>
+ 
+ 
+  <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdoyJcsyyiOo-t-you3J4zY9B8Edq3HG9iSPSYKWfpAC30Drw/viewform?embedded=true" width="640" height="943" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
   
  
 </div>
