7d0b1eb44c31b12f021f2d1d2c0fb297bb9f5e9b
Patrick McGuire <insidenothing@gmail.com>
2021-06-14 06:07:16 -0400

    Update bounties.php

--- bounties.php
+++ bounties.php
@@ -1,8 +1,8 @@
 <?PHP $title = 'Collect Bounties for $BALT'; include_once('menu.php'); ?>
+<div style='background-color:#15243c; text-align:center;'>
+ <img src='BMoreToken_1500x200.png' class="img-fluid" alt="Responsive image">
+</div> 
 <div class="container">
- <div style='background-color:#15243c; text-align:center;'>
-  <img src='BMoreToken_1500x200.png' class="img-fluid" alt="Responsive image">
- </div> 
  <center>    
    <div class="row justify-content-md-center">
      <h1>Paid Offers:</h1>
