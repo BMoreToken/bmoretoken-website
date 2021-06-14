e62b327ee03802ce432c53f3cb38277d948ab8a8
Patrick McGuire <insidenothing@gmail.com>
2021-06-14 05:49:09 -0400

    Create roadmap.php

---
+++ roadmap.php
@@ -0,0 +1,27 @@
+<?PHP $title = 'Project Roadmap for $BALT'; include_once('menu.php'); ?>
+
+
+<div class="container">
+ 
+<center>    
+ <img src='BMoreToken_256x256.png' class="img-fluid" alt="Responsive image">
+
+
+    <div class="row justify-content-md-center">
+      <h1>Directions</h1>
+      <h3>https://coinmarketcap.com</h3>
+      <h3>https://www.coingecko.com</h3>
+      <h3>AnySwap</h3>
+      <li>Needs CG Link</li>
+      <h3>https://etherscan.io/</h3>
+      <li>Needs Project Description Long</li>
+      <li>Needs Project Description Short</li>
+      <li>Needs Project Team</li>
+      
+    </div>
+  </center>
+  
+  
+  
+  </div>
+
