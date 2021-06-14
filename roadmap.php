617a4e9080e8e1da59d7db7b92f001cace69ad4c
Patrick McGuire <insidenothing@gmail.com>
2021-06-14 08:09:48 -0400

    Update roadmap.php

--- roadmap.php
+++ roadmap.php
@@ -2,18 +2,22 @@
 <div class="container">
  <center>    
     <div class="row justify-content-md-center">
-      <h1>Active Projects This Week</h1>
-      <h3>https://coinmarketcap.com</h3>
-      <h3>https://www.coingecko.com</h3>
-      <h3>AnySwap</h3>
+      <h1>Active Projects This Week (Last Contact)</h1>
+      <h3>https://coinmarketcap.com  (6/8/21)</h3>
+      <h3>https://www.coingecko.com  (6/8/21)</h3>
+      <h3>AnySwap (6/8/21)</h3>
       <li>Needs CG Link</li>
-      <h3>https://etherscan.io/</h3>
-      <li>Needs Project Description Long</li>
-      <li>Needs Project Description Short</li>
-      <li>Needs Project Team</li>
+      <li>Make Sure it Comes from Official Project E-Mail Address</li>
+      <h3>https://etherscan.io (6/13/21)</h3>
+      <li>Needs Clearer Project Description - Long</li>
+      <li>Needs Clearer Project Description - Short</li>
+      <li>Needs Better Defined Project Team</li>
+      <li>Needs Much More Activity on Public Social Media</li>
      <h1>Ideas</h1>
      <li>Minable Coin to interact with: BALTx</li>
      <li>Payment Gateway for Businesses</li>
+     <li>Burn x BALT when O's or Ravens Win</li>
     </div>
   </center>
 </div>
+<?PHP include_once('footer.php');
