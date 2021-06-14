acccb2ee3720422ba837dd851494315e5a1bd517
Patrick McGuire <insidenothing@gmail.com>
2021-06-14 06:05:17 -0400

    Update bounties.php

--- bounties.php
+++ bounties.php
@@ -1,24 +1,18 @@
 <?PHP $title = 'Collect Bounties for $BALT'; include_once('menu.php'); ?>
-
-
 <div class="container">
- 
-<center>    
- <img src='BMoreToken_1500x200.png' class="img-fluid" alt="Responsive image">
-
-
-    <div class="row justify-content-md-center">
+ <div style='background-color:#15243c; text-align:center;'>
+  <img src='BMoreToken_1500x200.png' class="img-fluid" alt="Responsive image">
+ </div> 
+ <center>    
+   <div class="row justify-content-md-center">
      <h1>Paid Offers:</h1>
-     <h3>New Logo Set 1500x200 500x500, 256x256, 32x32</h3>
-     <h3>Static Website Redesign, bootstrap@5</h3>
-     <h3>Edit Project Writeup ~500 words</h3>
-     <h3>Edit Project Tagline ~20 words</h3>
-     <h3>Referral of new team member</h3>
-     <h3>Promotions</h3>
-        
+       <h3>New Logo Set 1500x200 500x500, 256x256, 32x32</h3>
+       <h3>Static Website Redesign, bootstrap@5</h3>
+       <h3>Edit Project Writeup ~500 words</h3>
+       <h3>Edit Project Tagline ~20 words</h3>
+       <h3>Referral of new team member</h3>
+       <h3>Promotions</h3>   
     </div>
  </center>
 </div>
-
-  
-<?PHP include_once('footer.php'); ?>
+<?PHP include_once('footer.php'); 
