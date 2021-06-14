9e69a4f26b3e802b6e49fbbe7687e8005b882478
Patrick McGuire <insidenothing@gmail.com>
2021-06-14 07:53:26 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,16 +3,16 @@ $title = 'BMoreToken Project Home (BALT)';
 include_once('menu.php');
 ?>
 <div class="container">
- 
 <center>    
+ <p>Baltimore Time: <?PHP echo date('r'); ?></p>
  <p>Max Total Supply: 3,947,960 BALT</p>
- <p>Current Circulation: 8,117.07 BALT</p>
+ <p>Current Circulation: 6,590 BALT</p>
  <p>Holders: 6</p>
  <p>Transfers: 14</p>
  <p>Address: 0xbdd4f273c2b0f0b84a51bd733aac617d91159376</p>
+ <p>Liquidity: 5,790 BALT &amp; 1.135 WETH</p>
 </pre>
  </center>
-
     <div class="row justify-content-md-center">
      <div class="col-md-auto">
        <a type="button" class="btn btn-primary btn-lg btn-block" href='https://etherscan.io/token/0xbdd4f273c2b0f0b84a51bd733aac617d91159376'>EtherScan</a>
