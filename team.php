41a381772ae86dd4902e4831af65e3a7668e836e
Patrick McGuire <insidenothing@gmail.com>
2021-06-14 06:29:17 -0400

    Update team.php

--- team.php
+++ team.php
@@ -1,23 +1,14 @@
 <?PHP include_once('menu.php'); ?>
-
-
 <div class="container">
-
- 
- 
- <table>
-  <tr>
-   <td><img src="1537101314639.jpg"></td>
-   <td><a type="button" class="btn btn-info btn-lg btn-block" href='https://www.linkedin.com/in/baltimorehacker'>Patrick McGuire<br>Finance and Technology</a></td>
-  </tr>
- </table>
- 
- 
-  <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdoyJcsyyiOo-t-you3J4zY9B8Edq3HG9iSPSYKWfpAC30Drw/viewform?embedded=true" width="640" height="943" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
-  
- 
+ <center>
+  <table>
+   <tr>
+    <td valign="top"><img src="1537101314639.jpg"></td>
+    <td valign="top"><a type="button" class="btn btn-info btn-lg btn-block" href='https://www.linkedin.com/in/baltimorehacker'>Patrick McGuire<br>Finance and Technology</a></td>
+   </tr><tr>
+    <td colspan="2" valign="top"><iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdoyJcsyyiOo-t-you3J4zY9B8Edq3HG9iSPSYKWfpAC30Drw/viewform?embedded=true" width="640" height="1036" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe></td>
+   </tr>
+  </table>
+ </center>
 </div>
-
-
-
-<?PHP include_once('footer.php'); ?>
+<?PHP include_once('footer.php');
