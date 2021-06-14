5303b7659a3f7b71981d65ef06ac47e7a5ca15d1
Patrick McGuire <insidenothing@gmail.com>
2021-06-14 08:33:26 -0400

    Update bounties.php

--- bounties.php
+++ bounties.php
@@ -1,7 +1,11 @@
 <?PHP $title = 'Collect Bounties for $BALT'; include_once('menu.php'); ?>
+<style>
+ @media (min-width:1025px) { .container { width: 50% !important;} }
+</style>
+<center>
 <div class="container">
  <center>    
-   <div class="row justify-content-md-center">
+    <div class="row" style='text-align:left;'>
      <h1>Paid Offers: ( lack of time or skill so we outsource )</h1>
        <h3>New Logo Set 1500x200 500x500, 256x256, 32x32</h3>
        <h3>Static Website Redesign, bootstrap@5</h3>
