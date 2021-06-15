38a7a010a91294081073a5a8da92aa4efce2ac90
Patrick McGuire <insidenothing@gmail.com>
2021-06-15 14:35:29 -0400

    Update team.php

--- team.php
+++ team.php
@@ -1,12 +1,18 @@
 <?PHP include_once('menu.php'); ?>
 <div class="container">
  <center>
+  <br><br>
   <table>
    <tr>
     <td valign="top"><img src="1537101314639.jpg"></td>
-    <td valign="top"><a type="_Blank" type="button" class="btn btn-info btn-lg btn-block" href='https://www.linkedin.com/in/baltimorehacker'>Patrick McGuire<br>Finance and Technology</a></td>
+    <td valign="top"><h3>Patrick McGuire<br>Finance and Technology</h3><a type="_Blank" type="button" class="btn btn-info btn-lg btn-block" href='https://www.linkedin.com/in/baltimorehacker'>linkedin</a></td>
+   </tr>
+   <tr>
+    <td valign="top"><img src="1528373984428.jpg"></td>
+    <td valign="top"><h3>JT Runner II<br>Media Relations</h3><a type="_Blank" type="button" class="btn btn-info btn-lg btn-block" href='https://www.linkedin.com/in/jt-runner-ii-29bb65131'>linkedin</a></td>
    </tr>
   </table>
  </center>
+<br><br>
 </div>
 <?PHP include_once('footer.php');
