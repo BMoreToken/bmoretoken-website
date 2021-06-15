094b5575c34af36f6e818d33fd813e4fd3621c58
Patrick McGuire <insidenothing@gmail.com>
2021-06-15 07:01:03 -0400

    Update trade.php

--- trade.php
+++ trade.php
@@ -10,11 +10,11 @@ include_once('menu.php');
     <div class="row justify-content-md-center">
       
       <div class="col-md-auto">
-        <a type="button" class="btn btn-success btn-lg btn-block" href='https://exchange.sushiswapclassic.org/#/add/0xbdd4f273c2b0f0b84a51bd733aac617d91159376/ETH'>SushiSwap Add Liquidity</a>
+        <a target='_Blank' type="button" class="btn btn-success btn-lg btn-block" href='https://exchange.sushiswapclassic.org/#/add/0xbdd4f273c2b0f0b84a51bd733aac617d91159376/ETH'>SushiSwap Add Liquidity</a>
       </div>
       
       <div class="col-md-auto">
-        <a type="button" class="btn btn-success btn-lg btn-block" href='https://app.sushi.com/swap?inputCurrency=0xbdd4f273c2b0f0b84a51bd733aac617d91159376'>SushiSwap Trade</a>
+        <a target='_Blank' type="button" class="btn btn-success btn-lg btn-block" href='https://app.sushi.com/swap?inputCurrency=0xbdd4f273c2b0f0b84a51bd733aac617d91159376'>SushiSwap Trade</a>
       </div>
    
     </div>
