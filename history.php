66d65ef2f6cb49625a57e6735c288f422c6f3ded
Patrick McGuire <insidenothing@gmail.com>
2021-06-15 13:07:05 -0400

    Create history.php

---
+++ history.php
@@ -0,0 +1,21 @@
+<?PHP $title = 'Project History for $BALT'; include_once('menu.php'); ?>
+<style>
+ @media (min-width:1025px) { .container { width: 50% !important;} }
+</style>
+<center>
+<div class="container">
+ <center>    
+    <div class="row" style='text-align:left;'>
+      <li>2021-06-07 Deploy Contract</li>
+      <li>2021-06-08 Liquidity Pool</li>
+      <li>2021-06-09 Domain, E-Mail, Web</li>
+      <li>2021-06-10 SSL, Web3</li>
+      <li>2021-06-11 List Bounties </li>
+      <li>2021-06-12 Increase Liquidity</li>
+      <li>2021-06-13 Social Media Day</li>
+      <li>2021-06-14 Google form to Join Team</li>
+      <li>2021-06-15 History Page</li>
+    </div>
+</div>
+</center>
+<?PHP include_once('footer.php');
