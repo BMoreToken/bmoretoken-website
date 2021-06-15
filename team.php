187b37ba1de7883ff582a5f6ff82e076bfe9b9aa
Patrick McGuire <insidenothing@gmail.com>
2021-06-15 14:48:01 -0400

    Update team.php

--- team.php
+++ team.php
@@ -8,8 +8,8 @@
     <td valign="top" align="right"><h3>Patrick McGuire<br>Finance and Technology</h3><a type="_Blank" type="button" class="btn btn-info btn-lg btn-block" href='https://www.linkedin.com/in/baltimorehacker'>linkedin</a></td>
    </tr>
    <tr>
-    <td valign="top" align="left"><h3>JT Runner II<br>Media Relations</h3><a type="_Blank" type="button" class="btn btn-info btn-lg btn-block" href='https://www.linkedin.com/in/jt-runner-ii-29bb65131'>linkedin</a></td>
-    <td valign="top" align="right"><img src="1528373984428.jpg"></td>
+    <td valign="bottom" align="left"><h3>JT Runner II<br>Media Relations</h3><a type="_Blank" type="button" class="btn btn-info btn-lg btn-block" href='https://www.linkedin.com/in/jt-runner-ii-29bb65131'>linkedin</a></td>
+    <td valign="bottom" align="right"><img src="1528373984428.jpg"></td>
    </tr>
   </table>
  </center>
