f61dee12dd4093dbcb9910583c8b280c16c3e71b
Patrick McGuire <insidenothing@gmail.com>
2021-06-15 12:05:59 -0400

    Update team.php

--- team.php
+++ team.php
@@ -5,8 +5,6 @@
    <tr>
     <td valign="top"><img src="1537101314639.jpg"></td>
     <td valign="top"><a type="_Blank" type="button" class="btn btn-info btn-lg btn-block" href='https://www.linkedin.com/in/baltimorehacker'>Patrick McGuire<br>Finance and Technology</a></td>
-   </tr><tr>
-    <td colspan="2" valign="top"><iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdoyJcsyyiOo-t-you3J4zY9B8Edq3HG9iSPSYKWfpAC30Drw/viewform?embedded=true" width="640" height="1036" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe></td>
    </tr>
   </table>
  </center>
