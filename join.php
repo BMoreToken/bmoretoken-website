052cccebb66f3026e78729fe3ca1ffd884b735c4
Patrick McGuire <insidenothing@gmail.com>
2021-06-15 12:05:41 -0400

    Create join.php

---
+++ join.php
@@ -0,0 +1,7 @@
+<?PHP include_once('menu.php'); ?>
+<div class="container">
+ <center>
+    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdoyJcsyyiOo-t-you3J4zY9B8Edq3HG9iSPSYKWfpAC30Drw/viewform?embedded=true" width="640" height="1036" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe></td>
+  </center>
+</div>
+<?PHP include_once('footer.php');
