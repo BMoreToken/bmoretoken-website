3d07805e1e8d809f7eb535161adeaef915747693
Patrick McGuire <insidenothing@gmail.com>
2021-06-16 10:31:48 -0400

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,8 @@ include_once('menu.php');
  <p>Baltimore Time: <?PHP echo date('r'); ?></p>
  <p>Max Total Supply: 3,947,960 BALT</p>
  <p>Current Circulation: 6,690 BALT</p>
- <p>Holders: 6</p>
- <p>Transfers: 14</p>
+ <p>Holders: 7</p>
+ <p>Transfers: 15</p>
  <p>Address: 0xbdd4f273c2b0f0b84a51bd733aac617d91159376</p>
  <p>Liquidity: 5,790 BALT &amp; 1.135 WETH</p>
 </pre>
