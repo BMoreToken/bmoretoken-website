38e5cea802a804978a0b8c89a178ff72fa674753
Patrick McGuire <insidenothing@gmail.com>
2021-06-17 16:32:18 -0400

    Create liquidity_bonus.php

---
+++ liquidity_bonus.php
@@ -0,0 +1,18 @@
+
+<?PHP $title = 'Liquidity Bonus WETH-BALT'; include_once('menu.php'); ?>
+<style>
+ @media (min-width:1025px) { .container { width: 50% !important;} }
+</style>
+<center>
+<div class="container">
+ <center>    
+    <div class="row" style='text-align:left; height:350px;'>
+      <h1>2:1 Daily Liquidity Bonus</h1>
+      <p>Add 100 BALT Liquidity get 100 BALT</p>
+      <p>Liquidity Added Here: https://app.sushi.com/add/ETH/0xbDd4f273C2b0f0B84a51BD733AAc617D91159376</p>
+      <p>Monitor Here: https://etherscan.io/token/0xbdd4f273c2b0f0b84a51bd733aac617d91159376?a=0x08cd7cf527330f10571fb7947256bf0c8b38afc4</p>
+      <p>There is nothing for you to do, this is completely automated.</p>
+    </div>
+  </center>
+</div>
+<?PHP include_once('footer.php');
