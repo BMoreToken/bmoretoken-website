5eb6fbdc3355cfae77c7e1c487bd97a402cde8d6
Patrick McGuire <insidenothing@gmail.com>
2021-06-19 10:42:10 -0400

    Update bulksender.js

--- bulksender.js
+++ bulksender.js
@@ -3,9 +3,7 @@ const fs = require('fs')
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'content-type': 'text/html' })
-  fs.createReadStream('header.html').pipe(res)
   fs.createReadStream('bulksender.html').pipe(res)
-  fs.createReadStream('footer.html').pipe(res)
 })
 
 server.listen(process.env.PORT || 3001)
