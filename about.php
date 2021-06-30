7d65b5652e93e2a01f1fbee00f2a250b11805f8f
Patrick McGuire <insidenothing@gmail.com>
2021-06-30 06:34:43 -0400

    Create about.php

---
+++ about.php
@@ -0,0 +1,21 @@
+<?PHP $title = 'All About $BALT'; include_once('menu.php'); ?>
+<style>
+ @media (min-width:1025px) { .container { width: 50% !important;} }
+</style>
+<center>
+<div class="container">
+  <center>    
+    <div class="row" style='text-align:left;'>
+      <a type="button" class="btn btn-info btn-lg btn-block" href='https://www.bmoretoken.com/bounties.php'>Bounties</a><hr>
+      
+      <a type="button" class="btn btn-info btn-lg btn-block" href='https://www.bmoretoken.com/team.php'>Current Team</a><br>
+      <a type="button" class="btn btn-info btn-lg btn-block" href='https://www.bmoretoken.com/join.php'>Team Application</a><hr>
+     
+      <a type="button" class="btn btn-info btn-lg btn-block" href='https://www.bmoretoken.com/roadmap.php'>Future Roadmap</a><br><br>
+      <a type="button" class="btn btn-info btn-lg btn-block" href='https://www.bmoretoken.com/history.php'>Project History</a><hr>
+      
+      <a type="button" class="btn btn-info btn-lg btn-block" href='https://www.bmoretoken.com/contact.php'>Contact Information</a><br><br>
+    </div>
+  </center>
+</div>
+<?PHP include_once('footer.php');
