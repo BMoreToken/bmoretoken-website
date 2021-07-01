dc405d3b211de9807d5ee3dac145bdbb033d872d
Patrick McGuire <insidenothing@gmail.com>
2021-07-01 19:38:46 -0400

    Update liquidity_bonus.php

--- liquidity_bonus.php
+++ liquidity_bonus.php
@@ -11,7 +11,7 @@
       <p>Add 100 BALT/ETH Liquidity and Get 100 $BALT tokens ( double what liquidity you provide )</p>
       <p>ETH Liquidity Added Here: <a target='_Blank' href='https://app.sushi.com/add/ETH/0xbDd4f273C2b0f0B84a51BD733AAc617D91159376'>https://app.sushi.com/add/ETH/0xbDd4f273C2b0f0B84a51BD733AAc617D91159376</a></p>
       <p>Confirmed Here: <a target='_Blank' href='https://etherscan.io/token/0xbdd4f273c2b0f0b84a51bd733aac617d91159376?a=0x08cd7cf527330f10571fb7947256bf0c8b38afc4'>https://etherscan.io/token/0xbdd4f273c2b0f0b84a51bd733aac617d91159376?a=0x08cd7cf527330f10571fb7947256bf0c8b38afc4</a></p>
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScRVIeJYopQPXMbwSuTQDg5co0P0upg2tj0IfEKYOSPd6_k-Q/viewform?embedded=true" width="300" height="200" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScRVIeJYopQPXMbwSuTQDg5co0P0upg2tj0IfEKYOSPd6_k-Q/viewform?embedded=true" width="300" height="600" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
     </div>
   </center>
 </div>
