b0a12d87dd9530864aabfb2cd732e8101ee8a718
Patrick McGuire <insidenothing@gmail.com>
2021-07-01 08:50:51 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+console.log('Time: ', Date.now());
 const http = require('http')
 const fs = require('fs')
 const server = http.createServer((req, res) => {
@@ -5,13 +6,16 @@ const server = http.createServer((req, res) => {
   fs.createReadStream('header.html').pipe(res)
   var express = require('express')
   var app = express()
-  app.get('/tools/metamask', function (req, res) {
+  app.get('/tools/metamask', function (req, res) {  
+    console.log('Tool: MetaMask ', Date.now());
     fs.createReadStream('metamask.html').pipe(res)
   })
   app.get('/tools/bulksender', function (req, res) {
+    console.log('Tool: BulkSender ', Date.now());
     fs.createReadStream('bulksender.html').pipe(res)
   })
   app.get('/tools/events', function (req, res) {
+    console.log('Tool: Events ', Date.now());
     fs.createReadStream('calendar.html').pipe(res)
   })
   fs.createReadStream('footer.html').pipe(res)
