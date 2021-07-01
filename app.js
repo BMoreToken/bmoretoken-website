413c6ea1704504125ba2ea599a6e55376005a460
Patrick McGuire <insidenothing@gmail.com>
2021-07-01 08:14:05 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-//this file will be the only node instance, and we will look at the url to pick which html page to use; bulksender.html metamask.html events.html
 const http = require('http')
 const fs = require('fs')
-
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'content-type': 'text/html' })
   fs.createReadStream('header.html').pipe(res)
-  fs.createReadStream('bulksender.html').pipe(res)
+  var express = require('express')
+  var app = express()
+  app.get('/metamask', function (req, res) {
+    fs.createReadStream('metamask.html').pipe(res)
+  })
+  app.get('/bulksender', function (req, res) {
+    fs.createReadStream('bulksender.html').pipe(res)
+  })
+  app.get('/events', function (req, res) {
+    fs.createReadStream('calendar.html').pipe(res)
+  })
   fs.createReadStream('footer.html').pipe(res)
 })
-
-server.listen(process.env.PORT || 3001)
+server.listen(process.env.PORT || 3000)
