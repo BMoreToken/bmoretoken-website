6375cfc4a83d500a76410790d5026c2a7b34765f
Patrick McGuire <insidenothing@gmail.com>
2021-07-01 12:13:09 -0400

    Update app.js

--- app.js
+++ app.js
@@ -11,34 +11,38 @@ var html_footer = Object.create(null);
 fs.readFile("footer.html", "utf-8", function(error, data) {
   html_footer = data.toString();
 });
+// Tools Index
+var html_tools = Object.create(null);
+fs.readFile("tools.html", "utf-8", function(error, data) {
+  html_tools = data.toString();
+});
+// Basic MetaMask Test
+var html_metamask = Object.create(null);
+fs.readFile("metamask.html", "utf-8", function(error, data) {
+  html_metamask = data.toString();
+});
+// BulkSend Token
+var html_bulk = Object.create(null);
+fs.readFile("bulksender.html", "utf-8", function(error, data) {
+  html_bulk = data.toString();
+});
+// Event Calendar
+var html_events = Object.create(null);
+fs.readFile("calendar.html", "utf-8", function(error, data) {
+  html_events = data.toString();
+});
 // Routing 
 app.all('/', function (req, res) {
-  var html_main = Object.create(null);
-  fs.readFile("tools.html", "utf-8", function(error, data) {
-    html_main = data.toString();
-  });
-  res.send( html_header + html_main + html_footer );
+  res.send( html_header + html_tools + html_footer );
 });
 app.all('/metamask', function (req, res) {
-  var html_main = Object.create(null);
-  fs.readFile("metamask.html", "utf-8", function(error, data) {
-    html_main = data.toString();
-  });
-  res.send(html_header + html_main + html_footer);
+  res.send(html_header + html_metamask + html_footer);
 });
 app.all('/bulksender', function (req, res) {
-  var html_main = Object.create(null);
-  fs.readFile("bulksender.html", "utf-8", function(error, data) {
-    html_main = data.toString();
-  });
-  res.send(html_header + html_main + html_footer);
+  res.send(html_header + html_bulk + html_footer);
 });
 app.all('/events', function (req, res) {
-  var html_main = Object.create(null);
-  fs.readFile("calendar.html", "utf-8", function(error, data) {
-    html_main = data.toString();
-  });
-  res.send(html_header + html_main + html_footer);
+  res.send(html_header + html_events + html_footer);
 });
 // Start Server
 app.listen(3000, function () {
