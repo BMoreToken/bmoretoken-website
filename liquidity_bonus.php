6a673e24ab140c41524172607dc0c4e8003dcbb5
Patrick McGuire <insidenothing@gmail.com>
2021-07-01 19:29:01 -0400

    Update liquidity_bonus.php

--- liquidity_bonus.php
+++ liquidity_bonus.php
@@ -8,10 +8,10 @@
  <center>    
     <div class="row" style='text-align:left; height:350px;'>
       <h1>2:1 Daily Liquidity Bonus</h1>
-      <p>Add 100 BALT Liquidity get 100 BALT</p>
-      <p>Liquidity Added Here: <a target='_Blank' href='https://app.sushi.com/add/ETH/0xbDd4f273C2b0f0B84a51BD733AAc617D91159376'>https://app.sushi.com/add/ETH/0xbDd4f273C2b0f0B84a51BD733AAc617D91159376</a></p>
-      <p>Monitored Here: <a target='_Blank' href='https://etherscan.io/token/0xbdd4f273c2b0f0b84a51bd733aac617d91159376?a=0x08cd7cf527330f10571fb7947256bf0c8b38afc4'>https://etherscan.io/token/0xbdd4f273c2b0f0b84a51bd733aac617d91159376?a=0x08cd7cf527330f10571fb7947256bf0c8b38afc4</a></p>
-      <p>There is nothing for you to do, this is completely automated daily.</p>
+      <p>Add 100 BALT/ETH Liquidity and Get 100 $BALT tokens</p>
+      <p>ETH Liquidity Added Here: <a target='_Blank' href='https://app.sushi.com/add/ETH/0xbDd4f273C2b0f0B84a51BD733AAc617D91159376'>https://app.sushi.com/add/ETH/0xbDd4f273C2b0f0B84a51BD733AAc617D91159376</a></p>
+      <p>Confirmed Here: <a target='_Blank' href='https://etherscan.io/token/0xbdd4f273c2b0f0b84a51bd733aac617d91159376?a=0x08cd7cf527330f10571fb7947256bf0c8b38afc4'>https://etherscan.io/token/0xbdd4f273c2b0f0b84a51bd733aac617d91159376?a=0x08cd7cf527330f10571fb7947256bf0c8b38afc4</a></p>
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScRVIeJYopQPXMbwSuTQDg5co0P0upg2tj0IfEKYOSPd6_k-Q/viewform?embedded=true" width="300" height="200" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
     </div>
   </center>
 </div>
