d410222c349af75635f2c149fcddba1ba6927d62
Patrick McGuire <insidenothing@gmail.com>
2021-07-01 18:30:01 -0400

    Update trade.php

--- trade.php
+++ trade.php
@@ -29,22 +29,22 @@ include_once('menu.php');
  </div>
  
   <div class="row justify-content-md-center">
-    <h3>Sell For: ( updating links in 5 min )</h3>
+    <h3>Sell For:</h3>
     
       <div class="col-md-auto">
-        <a target='_Blank' type="button" class="btn btn-success btn-lg btn-block" href='https://app.sushi.com/swap?inputCurrency=ETH&outputCurrency=0xbDd4f273C2b0f0B84a51BD733AAc617D91159376'>ETH at SushiSwap</a>
+        <a target='_Blank' type="button" class="btn btn-success btn-lg btn-block" href='https://app.sushi.com/swap?inputCurrency=0xbDd4f273C2b0f0B84a51BD733AAc617D91159376'>ETH at SushiSwap</a>
       </div>
    
     <div class="col-md-auto">
-        <a target='_Blank' type="button" class="btn btn-primary btn-lg btn-block" href='https://matcha.xyz/markets/1/0xbdd4f273c2b0f0b84a51bd733aac617d91159376/0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'>ETH at Matcha by 0x</a>
+        <a target='_Blank' type="button" class="btn btn-primary btn-lg btn-block" href='https://matcha.xyz/markets/1/0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee/0xbdd4f273c2b0f0b84a51bd733aac617d91159376'>ETH at Matcha by 0x</a>
       </div>
      
     <div class="col-md-auto">
-        <a target='_Blank' type="button" class="btn btn-success btn-lg btn-block" href='https://app.sushi.com/swap?inputCurrency=0xdAC17F958D2ee523a2206206994597C13D831ec7&outputCurrency=0xbDd4f273C2b0f0B84a51BD733AAc617D91159376'>USDT at SushiSwap</a>
+        <a target='_Blank' type="button" class="btn btn-success btn-lg btn-block" href='https://app.sushi.com/swap?outputCurrency=0xdAC17F958D2ee523a2206206994597C13D831ec7&inputCurrency=0xbDd4f273C2b0f0B84a51BD733AAc617D91159376'>USDT at SushiSwap</a>
       </div>
     
      <div class="col-md-auto">
-        <a target='_Blank' type="button" class="btn btn-primary btn-lg btn-block" href='https://matcha.xyz/markets/1/0xbdd4f273c2b0f0b84a51bd733aac617d91159376/0xdac17f958d2ee523a2206206994597c13d831ec7'>USDT at Matcha by 0x</a>
+        <a target='_Blank' type="button" class="btn btn-primary btn-lg btn-block" href='https://matcha.xyz/markets/1/0xdac17f958d2ee523a2206206994597c13d831ec7/0xbdd4f273c2b0f0b84a51bd733aac617d91159376'>USDT at Matcha by 0x</a>
       </div>
     
  </div>
@@ -53,12 +53,17 @@ include_once('menu.php');
   <br> <br>
     <div class="row justify-content-md-center">
       
+      <h3>Liquidity:</h3>
+     
       <div class="col-md-auto">
-        <a target='_Blank' type="button" class="btn btn-success btn-lg btn-block" href='https://exchange.sushiswapclassic.org/#/add/0xbdd4f273c2b0f0b84a51bd733aac617d91159376/ETH'>Add Liquidity</a>
+        <a target='_Blank' type="button" class="btn btn-warning btn-lg btn-block" href='https://exchange.sushiswapclassic.org/#/add/0xbdd4f273c2b0f0b84a51bd733aac617d91159376/ETH'>Add Liquidity</a>
       </div>
       
    
-    
+    <div class="col-md-auto">
+        <a target='_Blank' type="button" class="btn btn-warning btn-lg btn-block" href='/liquidity_bonus.php'>Liquidity Bonus Details</a>
+      </div>
+      
      
     </div>
 
