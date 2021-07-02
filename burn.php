ff981a1b54f351db7941c539abe3633b37b5befa
Patrick McGuire <insidenothing@gmail.com>
2021-07-02 08:22:33 -0400

    Create burn.php

---
+++ burn.php
@@ -0,0 +1,25 @@
+<?PHP $title = 'Project History for $BALT'; include_once('menu.php'); ?>
+<style>
+ @media (min-width:1025px) { .container { width: 50% !important;} }
+</style>
+<center>
+<div class="container">
+ <center>    
+    <div class="row" style='text-align:left;'>
+
+      <h1>If the Baltimore Orioles Win - We will burn their score of $BALT tokens<h1>
+
+      <h3>Next Chance for Burn - Tonight @ 7:05 PM</h3>
+
+      <a href='https://www.mlb.com/orioles/schedule/'>https://www.mlb.com/orioles/schedule/</a>
+
+      <h1>If the Baltimore Ravens Win - We will burn their score of $BALT tokens<h1>
+
+      <h3>Next Chance for Burn - Sat 08/14 @ 7:00 PM EDT</h3>
+
+      <a href='https://www.baltimoreravens.com/schedule/'>https://www.baltimoreravens.com/schedule/</a>
+
+    </div>
+</div>
+</center>
+<?PHP include_once('footer.php');
