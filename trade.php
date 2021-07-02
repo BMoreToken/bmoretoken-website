8300852dacefce48aececaf90d54fb9343cfeeb7
Patrick McGuire <insidenothing@gmail.com>
2021-07-02 12:32:35 -0400

    Update trade.php

--- trade.php
+++ trade.php
@@ -8,44 +8,38 @@ include_once('menu.php');
 <center>  
  
    <div class="row justify-content-md-center">
-    <h3>Buy With:</h3>
+    <h3>Buy</h3>
     
       <div class="col-md-auto">
-        <a target='_Blank' type="button" class="btn btn-success btn-lg btn-block" href='https://app.sushi.com/swap?inputCurrency=ETH&outputCurrency=0xbDd4f273C2b0f0B84a51BD733AAc617D91159376'>ETH at SushiSwap</a>
+        <a target='_Blank' type="button" class="btn btn-success btn-lg btn-block" href='https://app.sushi.com/swap?inputCurrency=ETH&outputCurrency=0xbDd4f273C2b0f0B84a51BD733AAc617D91159376'>SushiSwap</a>
       </div>
    
     <div class="col-md-auto">
-        <a target='_Blank' type="button" class="btn btn-primary btn-lg btn-block" href='https://matcha.xyz/markets/1/0xbdd4f273c2b0f0b84a51bd733aac617d91159376/0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'>ETH at Matcha by 0x</a>
+        <a target='_Blank' type="button" class="btn btn-primary btn-lg btn-block" href='https://matcha.xyz/markets/1/0xbdd4f273c2b0f0b84a51bd733aac617d91159376/0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'>Matcha</a>
       </div>
      
     <div class="col-md-auto">
-        <a target='_Blank' type="button" class="btn btn-success btn-lg btn-block" href='https://app.sushi.com/swap?inputCurrency=0xdAC17F958D2ee523a2206206994597C13D831ec7&outputCurrency=0xbDd4f273C2b0f0B84a51BD733AAc617D91159376'>USDT at SushiSwap</a>
+        <a target='_Blank' type="button" class="btn btn-info btn-lg btn-block" href='https://app.uniswap.org/#/swap?inputCurrency=0xbdd4f273c2b0f0b84a51bd733aac617d91159376'>Uniswap</a>
       </div>
     
-     <div class="col-md-auto">
-        <a target='_Blank' type="button" class="btn btn-primary btn-lg btn-block" href='https://matcha.xyz/markets/1/0xbdd4f273c2b0f0b84a51bd733aac617d91159376/0xdac17f958d2ee523a2206206994597c13d831ec7'>USDT at Matcha by 0x</a>
-      </div>
-    
+
  </div>
  
   <div class="row justify-content-md-center">
-    <h3>Sell For:</h3>
+    <h3>Sell</h3>
     
       <div class="col-md-auto">
-        <a target='_Blank' type="button" class="btn btn-success btn-lg btn-block" href='https://app.sushi.com/swap?inputCurrency=0xbDd4f273C2b0f0B84a51BD733AAc617D91159376'>ETH at SushiSwap</a>
+        <a target='_Blank' type="button" class="btn btn-success btn-lg btn-block" href='https://app.sushi.com/swap?inputCurrency=0xbDd4f273C2b0f0B84a51BD733AAc617D91159376'>SushiSwap</a>
       </div>
    
     <div class="col-md-auto">
-        <a target='_Blank' type="button" class="btn btn-primary btn-lg btn-block" href='https://matcha.xyz/markets/1/0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee/0xbdd4f273c2b0f0b84a51bd733aac617d91159376'>ETH at Matcha by 0x</a>
+        <a target='_Blank' type="button" class="btn btn-primary btn-lg btn-block" href='https://matcha.xyz/markets/1/0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee/0xbdd4f273c2b0f0b84a51bd733aac617d91159376'>Matcha</a>
       </div>
      
-    <div class="col-md-auto">
-        <a target='_Blank' type="button" class="btn btn-success btn-lg btn-block" href='https://app.sushi.com/swap?outputCurrency=0xdAC17F958D2ee523a2206206994597C13D831ec7&inputCurrency=0xbDd4f273C2b0f0B84a51BD733AAc617D91159376'>USDT at SushiSwap</a>
-      </div>
-    
      <div class="col-md-auto">
-        <a target='_Blank' type="button" class="btn btn-primary btn-lg btn-block" href='https://matcha.xyz/markets/1/0xdac17f958d2ee523a2206206994597c13d831ec7/0xbdd4f273c2b0f0b84a51bd733aac617d91159376'>USDT at Matcha by 0x</a>
+        <a target='_Blank' type="button" class="btn btn-info btn-lg btn-block" href='https://app.uniswap.org/#/swap?outputCurrency=0xbdd4f273c2b0f0b84a51bd733aac617d91159376'>Uniswap</a>
       </div>
+  
     
  </div>
  
@@ -60,7 +54,7 @@ include_once('menu.php');
       </div>
       
    <div class="col-md-auto">
-        <a target='_Blank' type="button" class="btn btn-success btn-lg btn-block" href='https://app.uniswap.org/#/add/0xbdd4f273c2b0f0b84a51bd733aac617d91159376'>Add Liquidity at Uniswap</a>
+        <a target='_Blank' type="button" class="btn btn-info btn-lg btn-block" href='https://app.uniswap.org/#/add/0xbdd4f273c2b0f0b84a51bd733aac617d91159376'>Add Liquidity at Uniswap</a>
       </div>
      
     <div class="col-md-auto">
