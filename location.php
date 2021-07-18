90bef74d034f5744775d1e0e7e3ce9555884290f
Patrick McGuire <insidenothing@gmail.com>
2021-07-18 16:37:37 -0400

    Update location.php

--- location.php
+++ location.php
@@ -1,13 +1,19 @@
 <?PHP
+require_once 'vendor/autoload.php';
+use GeoIp2\Database\Reader;
+
+
+
+
 // Get the data from client, assume location and return block reward value
 // I guess a VPN exit node in baltimore would be a workaround, any ideas to block that?
   function getIPAddress() {  
     //whether ip is from the share internet  
-     if(!emptyempty($_SERVER['HTTP_CLIENT_IP'])) {  
+     if(!empty($_SERVER['HTTP_CLIENT_IP'])) {  
                 $ip = $_SERVER['HTTP_CLIENT_IP'];  
         }  
     //whether ip is from the proxy  
-    elseif (!emptyempty($_SERVER['HTTP_X_FORWARDED_FOR'])) {  
+    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {  
                 $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];  
      }  
 //whether ip is from the remote address  
@@ -16,6 +22,25 @@
      }  
      return $ip;  
 }  
+
+
 $ip = getIPAddress();  
-echo 'IP Address - '.$ip.' - Location: ';
+echo 'IP Address - '.$ip;
+
+// This creates the Reader object, which should be reused across
+// lookups.
+$reader = new Reader('/usr/local/share/GeoIP/GeoIP2-ISP.mmdb');
+
+$record = $reader->isp($ip);
+
+print($record->autonomousSystemNumber . "\n"); // 217
+print($record->autonomousSystemOrganization . "\n"); // 'University of Minnesota'
+print($record->isp . "\n"); // 'University of Minnesota'
+print($record->organization . "\n"); // 'University of Minnesota'
+
+print($record->ipAddress . "\n"); // '128.101.101.101'
+print($record->network . "\n"); // '128.101.101.101/32'
+
+
+
 ?>
