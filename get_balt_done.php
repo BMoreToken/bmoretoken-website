26cdb9f03fe49a7ae6350307cde11ddb0203de21
Patrick McGuire <insidenothing@gmail.com>
2022-01-30 23:23:27 -0500

    Update get_balt_done.php

--- get_balt_done.php
+++ get_balt_done.php
@@ -2,12 +2,12 @@
 include_once("menu.php");
 if (isset($_GET['drop'])){ 
   if($_GET['drop'] == '1'){
-    $message = "Proper Address Received. Address Added for Next Friday's Drop. Thanks. Spread the Word!!!";
+    $message = "Proper Address Received. Address Added for the Next Drop. Thanks. Spread the Word!!!";
   }elseif($_GET['drop'] == '2'){
-    $message = "Address **NOT** Added for Next Friday's Drop. Maybe Try Again?";
+    $message = "Address **NOT** Added for the Next Drop. Maybe Try Again?";
   }
 }else{
-  $message = "Address **NOT** Added for Next Friday's Drop. Maybe Try Again?";
+  $message = "Address **NOT** Added for the Next Drop. Maybe Try Again?";
 }
 ?>
 
